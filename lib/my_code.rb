62e71808a4ce3241362f19b251f47ffd6c46d6a8
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-13 23:46:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,31 @@
 def map_to_negativize(source_array)
   
 end 
+
+def map_to_no_change(source_array)
+  
+end 
+
+def map_to_double(source_array)
+  i = 0 
+  while i < source_array.length 
+  
+  i += 1
+end 
+end 
+
+def map_to_square(source_array)
+  
+end 
+
+def reduce_to_total(source_array, starting_point)
+  
+end 
+
+def reduce_to_all_true(source_array)
+  
+end 
+
+def reduce_to_any_true(source_array)
+  
+end 
