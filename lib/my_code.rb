fe2cff672ce5e352ba589ac5156dfc1f75a89bf1
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 00:29:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def map_to_no_change(source_array)
    new_array = []
   i = 0 
   while i < source_array.size do
-  new_array.push(source_array[i] * -1)
+  new_array.push(source_array[i])
   i += 1
  end 
  new_array
