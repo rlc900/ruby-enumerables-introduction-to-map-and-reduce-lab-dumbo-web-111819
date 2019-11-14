52ed487ca6f4d8c51fc129d1b54ef3c12457e8b0
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 00:27:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ end
 def map_to_double(source_array)
   new_array = []
   i = 0 
-  while i < source_array.length 
+  while i < source_array.length do
   new_array.push(source_array[i] * -1)
   i += 1
  end 
