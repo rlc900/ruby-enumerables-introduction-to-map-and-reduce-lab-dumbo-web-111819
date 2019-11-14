a8c66ae0a7ae7db026ac4e7040eba1fda1b85c2f
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 00:30:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,13 @@ def map_to_no_change(source_array)
 end 
 
 def map_to_double(source_array)
- 
+  new_array = []
+  i = 0 
+  while i < source_array.size do
+  new_array.push(source_array[i] * 2)
+  i += 1
+ end 
+ new_array
 end 
 
 def map_to_square(source_array)
