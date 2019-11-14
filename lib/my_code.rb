5a7fe9fb1890a186036966bea249d327757acfea
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 00:54:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,11 +53,10 @@ def reduce_to_all_true(source_array)
   while i < source_array.size do
   if source_array[i] == false
     return false 
-  else
-    return true
   end 
   i += 1
  end 
+ return true
 end 
 
 def reduce_to_any_true(source_array)
